ac010d6762866bfca0a6d0dfa3f507723fb5a6f6
Marco Pollacci <marco.pollacci@gellify.com>
2025-02-03 12:50:59 +0100

    refactor(intro): :recycle: refactory style rtl

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,14 +1,23 @@
-const observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-    const isVisibile = mutation.target.getAttribute('aria-hidden') === 'false';
-    document.querySelectorAll('.example-inclusion-2 div span').forEach(el => {
-      const value = '1em';
-      el.style.marginRight = !isVisibile ? value : '';
-      el.style.marginInlineEnd = isVisibile ? value : '';
+const exampleTwo = () => {
+  const spanToCheck = Array.from(document.querySelectorAll('.example-inclusion-2 div span'));
+  const faces = spanToCheck.map(el => el.innerHTML);
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      const isVisibile = mutation.target.getAttribute('aria-hidden') === 'false';
+      spanToCheck.forEach((el, index) => {
+        const value = '1em';
+        el.style.marginRight = !isVisibile ? value : '';
+        el.style.marginInlineEnd = isVisibile ? value : '';
+        if (index === 1) {
+          el.innerHTML = faces[isVisibile ? 0 : 1];
+        }
+      });
     });
   });
-});
-observer.observe(document.querySelector('.example-inclusion-2 p-fragment.trigger'), {
-  attributes: true,
-  attributeFilter: ['aria-hidden']
-});
+  observer.observe(document.querySelector('.example-inclusion-2 p-fragment.trigger'), {
+    attributes: true,
+    attributeFilter: ['aria-hidden']
+  });
+};
+
+exampleTwo();
