a94750e9fa829cf39d2ed7fb779a852d64f7d133
Marco Pollacci <pollacci@gmail.com>
2025-02-03 18:39:24 +0100

    refactor(intro): :recycle: refactor behaviour rtl js

--- src/js/intro.js
+++ src/js/intro.js
@@ -4,19 +4,28 @@ const exampleTwo = () => {
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       const isVisibile = mutation.target.getAttribute('aria-hidden') === 'false';
+      const hasBorderRed = mutation.target.hasAttribute('border-red');
+      if (hasBorderRed) {
+        const divRtl = document.querySelector('.example-inclusion-2 div[dir="rtl"] span');
+        divRtl.style.borderColor = isVisibile ? '#ff0000' : '#ffffff';
+        return;
+      }
       spanToCheck.forEach((el, index) => {
         const value = '1em';
         el.style.marginRight = !isVisibile ? value : '';
         el.style.marginInlineEnd = isVisibile ? value : '';
         if (index === 1) {
           el.innerHTML = faces[isVisibile ? 0 : 1];
+          el.style.borderColor = '';
         }
       });
     });
   });
-  observer.observe(document.querySelector('.example-inclusion-2 p-fragment.trigger'), {
-    attributes: true,
-    attributeFilter: ['aria-hidden']
+  document.querySelectorAll('.example-inclusion-2 p-fragment.trigger').forEach(el => {
+    observer.observe(el, {
+      attributes: true,
+      attributeFilter: ['aria-hidden']
+    });
   });
 };
 
